eb0a9fb576ba9c3a316e898a1498c282a78c6a7e
yohanes-erwin <erwin.setiawan789@gmail.com>
2020-12-11 15:28:33 +0700

    First commit

---
+++ linear_regression_bike_sharing/bike_sharing_tf_js_esp32/bike_sharing_tf_js_esp32.ino
@@ -0,0 +1,60 @@
+#include <SPIFFS.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+
+const char* ssid = "Huawei-E5573";
+const char* password = "huaweie5573";
+
+AsyncWebServer server(80);
+
+void setup()
+{
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  // Initialize SPIFFS
+  if(!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for web page and libraries
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/index.html");
+  });
+  server.on("/bootstrap.min.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/bootstrap.min.css");
+  });
+  server.on("/jquery-3.3.1.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/jquery-3.3.1.min.js");
+  });
+  server.on("/popper.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/popper.min.js");
+  });
+  server.on("/bootstrap.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/bootstrap.min.js");
+  });
+  server.on("/tf.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/tf.min.js");
+  });
+
+  // Start server
+  server.begin();
+}
+ 
+void loop()
+{  
+}
+
